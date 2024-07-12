e5202636e43ae19adb6cdfa2d3a5d5f05d5bbbca
Antony-Rya <159479364+Antony-Rya@users.noreply.github.com>
2024-07-12 19:19:22 +0000

    q3

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 def q1():
-    pass
+    divisiveis = 0
+    numero = int(input(""))
+    for num in range(1, numero + 1):
+        if numero % num == 0 and num % 3 == 0:
+            divisiveis += 1
+    if divisiveis == 0:
+        print("O número não possui divisores multiplos de 3")
+    else:
+        print("Quantidade de divisores divisiveis por 3:", divisiveis)
 
 def q2():
     pass
