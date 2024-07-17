ec29c954cf9d7e7d81db11a6adc1b7ce2309aedb
Antony-Rya <159479364+Antony-Rya@users.noreply.github.com>
2024-07-17 17:48:51 +0000

    q4

--- main.py
+++ main.py
@@ -37,4 +37,14 @@ def q3():
     
 
 def q4():
-    pass
+    numero = int(input(""))
+    lista = []
+    soma = 0
+    exemplo = ""
+    for i in range(1, numero + 1):
+        exemplo = f"{i}/{i*3}"
+        lista.append(exemplo)
+        soma += i/i*3
+    print(lista)
+    print(soma)
+
